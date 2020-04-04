b7e0ae8e566b7b278a93824b437013328c72afd7
Nathan Johnson <nathan@tegroup.ca>
2020-04-04 12:44:07 -0600

    Character model update.

--- app/character/admin.py
+++ app/character/admin.py
@@ -3,7 +3,42 @@ from app.character import models
 
 
 class CharacterAdmin(admin.ModelAdmin):
-    list_display = ('user', 'character_class', 'name')
+    list_display = ('user', 'character_class', 'name', 'aspect', 'alignment')
 
 
 admin.site.register(models.Character, CharacterAdmin)
+
+
+class CharacterClassAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+admin.site.register(models.CharacterClass, CharacterClassAdmin)
+
+
+class AspectAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+admin.site.register(models.Aspect, AspectAdmin)
+
+
+class AlignmentAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+admin.site.register(models.Alignment, AlignmentAdmin)
+
+
+class PerkAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
+
+admin.site.register(models.Perk, PerkAdmin)
+
+
+class QuirkAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
+
+admin.site.register(models.Quirk, QuirkAdmin)
--- app/character/models.py
+++ app/character/models.py
@@ -2,13 +2,59 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Perk(models.Model):
+    name = models.CharField(max_length=32, default='')
+    description = models.TextField(default='')
+
+    def __str__(self):
+        return self.name;
+
+
+class Quirk(models.Model):
+    name = models.CharField(max_length=32, default='')
+    description = models.TextField(default='')
+
+    def __str__(self):
+        return self.name;
+
+
+class Aspect(models.Model):
+    name = models.CharField(max_length=32, default='')
+    description = models.TextField(default = '')
+    perk_1 = models.ForeignKey(Perk, on_delete=models.CASCADE, related_name='perk_1')
+    perk_2 = models.ForeignKey(Perk, on_delete=models.CASCADE, related_name='perk_2')
+    perk_3 = models.ForeignKey(Perk, on_delete=models.CASCADE, related_name='perk_3')
+    quirk_1 = models.ForeignKey(Quirk, on_delete=models.CASCADE, related_name='quirk_1')
+    quirk_2 = models.ForeignKey(Quirk, on_delete=models.CASCADE, related_name='quirk_2')
+    quirk_3 = models.ForeignKey(Quirk, on_delete=models.CASCADE, related_name='quirk_3')
+
+    def __str__(self):
+        return self.name;
+
+
+class Alignment(models.Model):
+    name = models.CharField(max_length=32, default='')
+    description = models.TextField(default = '')
+
+    def __str__(self):
+        return self.name;
+
+
 class CharacterClass(models.Model):
     name = models.CharField(max_length=16, default='')
+
+    def __str__(self):
+        return self.name;
 
 
 class Character(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     character_class = models.ForeignKey(CharacterClass, on_delete=models.CASCADE)
+    aspect = models.ForeignKey(Aspect, on_delete=models.CASCADE, blank=True, null=True)
+    alignment = models.ForeignKey(Alignment, on_delete=models.CASCADE, blank=True, null=True)
     name = models.CharField(max_length=16, default='')
+
+    def __str__(self):
+        return self.name;
 
 
